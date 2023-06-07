bccb3f70c2f04eea4d67d4f1c6ca9e1df85bdffd
Saron <saron@drcarmen.com>
2023-06-07 09:41:44 +0300

    Add TaskScheduler configuration

---
+++ boot/src/main/java/com/saron/spring/test/config/TaskSchedulerConfig.java
@@ -0,0 +1,16 @@
+package com.saron.spring.test.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+
+@Configuration
+public class TaskSchedulerConfig {
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        return new ThreadPoolTaskScheduler();
+    }
+
+}
